69ff568119129b7475bf8bce2cb41fd37471d7bd
cv47al <cv47al@8b615d0c-cabe-11dd-8772-d1b041bf0e37>
2008-12-15 15:49:50 +0000

    initial commit

---
+++ trunk/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Service Advertiser
+//
+//  Created by  Sven on 02.12.08.
+//  Copyright earthlingsoft 2008. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
